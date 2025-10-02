9d787dbf2bd3e419022441c2424cf00f240e918d
Edith <edith.argcc@ciencias.unam.mx>
2025-10-02 00:00:39 -0600

    Parte1

--- src/Practica02.hs
+++ src/Practica02.hs
@@ -8,14 +8,31 @@ type Binario = [Bit]
 
 --BINARIOS
 
+--Longitud de una lista
+--(usado en toDecimal)
+long :: [a] -> Int
+long [] = 0
+long (x:xs) = 1 + long xs
+
+--Pasa de Binario a Decimal
 toDecimal :: Binario -> Int
-toDecimal = undefined
+toDecimal [] = error "No se puede pasar a decimal"
+toDecimal [O] = 0
+toDecimal [I] = 1
+toDecimal (I:xs) = (2 ^ long xs) + toDecimal xs
+toDecimal (O:xs) = toDecimal xs
 
+--Pasa de Decimal a Binario
 toBin :: Int -> Binario
-toBin = undefined
+toBin 0 = [O]
+toBin 1 = [I]
+toBin n = if n `mod` 2 == 0 then toBin (n `div` 2) ++ [O] else toBin (n `div` 2) ++ [I]
 
+--Suma dos Binarios y devuelve un Bianrio
 suma :: Binario -> Binario -> Binario
-suma = undefined
+suma xs [] = xs
+suma [] ys = ys
+suma xs ys = toBin(toDecimal xs + toDecimal ys)
 
 --LISTAS
 
