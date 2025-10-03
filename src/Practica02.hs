557c5c1df31d119b5c91223007cece3275864d13
Franciscogh89 <franciscogh89@ciencias.unam.mx>
2025-10-03 16:58:58 -0600

    Practica completa

--- src/Practica02.hs
+++ src/Practica02.hs
@@ -8,11 +8,12 @@ type Binario = [Bit]
 
 --BINARIOS
 
+-- Funcion auxiliar
 --Longitud de una lista
 --(usado en toDecimal)
 long :: [a] -> Int
 long [] = 0
-long (x:xs) = 1 + long xs
+long (_:xs) = 1 + long xs
 
 --Pasa de Binario a Decimal
 toDecimal :: Binario -> Int
@@ -28,7 +29,7 @@ toBin 0 = [O]
 toBin 1 = [I]
 toBin n = if n `mod` 2 == 0 then toBin (n `div` 2) ++ [O] else toBin (n `div` 2) ++ [I]
 
---Suma dos Binarios y devuelve un Bianrio
+--Suma dos Binarios y devuelve un Binario
 suma :: Binario -> Binario -> Binario
 suma xs [] = xs
 suma [] ys = ys
@@ -36,16 +37,24 @@ suma xs ys = toBin(toDecimal xs + toDecimal ys)
 
 --LISTAS
 
-palindromo :: [a] -> Bool
-palindromo = undefined
+-- Función del ejercico 1 de la seccion "Listas", que verifica si una lista es palíndromo
+palindromo :: Eq a => [a] -> Bool
+palindromo xs = xs == reversa xs
 
 --Funcion principal que calcula la diferencia simetrica (Aquellos elementos que esten en la union pero no en la interseccion)
-diferenciaSimetrica :: [a] -> [a] -> [a]
-diferenciaSimetrica = undefined
+diferenciaSimetrica :: Eq a => [a] -> [a] -> [a]
+diferenciaSimetrica xs ys = concatena (diferencia xs ys) (diferencia ys xs)
 
 --Conjunto potencia
+-- Funcion del ejercicio 3 de la seccion "Listas", que calcula el conjunto potencia 
 conjuntoPotencia :: [a] -> [[a]]
-conjuntoPotencia = undefined
+conjuntoPotencia [] = [[]]  
+conjuntoPotencia (x:xs) = concatena subconjuntosSinX subconjuntosConX
+  where
+   -- Subconjuntos que no incluyen x
+    subconjuntosSinX = conjuntoPotencia xs
+    -- Subconjuntos que sí incluyen x
+    subconjuntosConX = agregaATodas x subconjuntosSinX
 
 
 --LISTAS DE LONGITUD PAR
@@ -54,16 +63,77 @@ type ListaPar a b = [(a,b)]
 
 --Longitud
 longitud :: ListaPar a b -> Int
-longitud = undefined
+longitud [] = 0
+longitud ((_,_):xs) = 2 + longitud xs
 
 --Map
 myMap :: (a -> c) -> (b -> d) -> ListaPar a b -> ListaPar c d 
-myMap = undefined
+myMap _ _ [] = []
+myMap f g ((a,b):xs) = (f a, g b):myMap f g xs
 
 --Sumar pares
-sumaPares :: ListaPar a b -> (a,b)
-sumaPares = undefined
+sumaPares :: (Num a, Num b) => ListaPar a b -> (a,b)
+sumaPares [] = (0, 0)
+sumaPares ((a,b):xs) = (a + sumaPrimer xs, b + sumaSegundo xs)
 
 --Filter pares
 myFilter :: ((a,b) -> Bool) -> ListaPar a b -> ListaPar a b
-myFilter = undefined
+myFilter _ [] = []
+myFilter f ((a, b):xs) = if f (a,b) then ((a,b):myFilter f xs) else (myFilter f xs)
+
+--Funciones auxiliares para LISTAS
+
+-- Reversa de una lista
+-- Se utiliza en la funcion palindromo
+reversa :: [a] -> [a]
+reversa [] = []
+reversa (x:xs) = concatena (reversa xs) [x] --O bien reversa xs ++ [x]
+
+-- Concatena dos listas
+-- Se utiliza en la funcion diferenciaSimetrica
+-- Se utiliza en la funcion conjuntoPotencia
+concatena :: [a] -> [a] -> [a]
+concatena [] ys = ys
+concatena (x:xs) ys = (x:concatena xs ys)
+
+-- Busca un elemento en una lista
+-- Se utiliza en la funcion auxiliar diferencia
+contiene :: (Eq a) => [a] -> a -> Bool
+contiene [] _ = False
+contiene (x:xs) y = if x == y then True else contiene xs y
+
+-- Diferencia de dos listas (elementos en xs pero no en ys)
+-- Se utiliza en la funcion diferenciaSimetrica
+diferencia :: Eq a => [a] -> [a] -> [a]
+diferencia [] _ = []
+diferencia (x:xs) ys
+    | contiene ys x = diferencia xs ys
+    | otherwise     = x : diferencia xs ys
+
+-- Agrega un elemento al inicio de cada lista en una lista de listas
+-- Se utiliza en la funcion conjuntoPotencia
+agregaATodas :: a -> [[a]] -> [[a]]
+agregaATodas _ [] = []
+agregaATodas x (ys:yss) = (x:ys) : agregaATodas x yss
+
+
+--Funciones auxiliares para LISTAS DE LONGITUD PAR
+
+-- sumaPrimer sirve para obtener el valor solo de los primeros elementos de la lista par
+-- Se utiliza para la funcion sumaPares
+sumaPrimer :: (Num a) => ListaPar a b -> a
+sumaPrimer [] = 0
+sumaPrimer ((a, _):xs) = a + sumaPrimer xs
+
+-- sumaSegundo sirve para obtener el valor solo de los segundos elementos de la lista par
+-- Se utiliza para la funcion sumaPares
+sumaSegundo :: (Num b) => ListaPar a b -> b
+sumaSegundo [] = 0
+sumaSegundo ((_, b):xs) = b + sumaSegundo xs
+
+
+
+----------------------------------------------
+--conjuntoPotencia :: [a] -> [[a]]
+--conjuntoPotencia [] = [[]]
+--conjuntoPotencia (x:xs) = [(x:ys) | ys <- conjuntoPotencia xs] ++ (conjuntoPotencia xs)
